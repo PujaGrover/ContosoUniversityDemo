a3746076bd8715744a37ee379ab1af05bc4babb0
Puja Grover <puja_grover@yahoo.com>
2019-06-12 23:57:19 -0500

    Learning Project for Week 21 Database

--- ContosoUniversityDemo/Controllers/HomeController.cs
+++ ContosoUniversityDemo/Controllers/HomeController.cs
@@ -5,11 +5,20 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ContosoUniversityDemo.Models;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversityDemo.Data;
+using ContosoUniversityDemo.Models.SchoolViewModels;
 
 namespace ContosoUniversityDemo.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly SchoolContext _context;
+
+        public HomeController(SchoolContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
@@ -24,6 +33,19 @@ namespace ContosoUniversityDemo.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task<ActionResult> About()
+        {
+            IQueryable<EnrollmentDateGroup> data =
+                from student in _context.Students
+                group student by student.EnrollmentDate into dateGroup
+                select new EnrollmentDateGroup()
+                {
+                    EnrollmentDate = dateGroup.Key,
+                    StudentCount = dateGroup.Count()
+                };
+            return View(await data.AsNoTracking().ToListAsync());
         }
     }
 }
---
+++ ContosoUniversityDemo/Models/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversityDemo.Models
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]//LET'S U ENTER Primary Key rather than havind database generate it
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
